246e6fa94628a1ac178f90df0bfe453fb2eea929
muhammadalisyed207-sudo <muhammadalisyed207@gmail.com>
2025-12-06 01:11:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+
+
+
+
+alert("welcome to my website ")
+
+
+alert("hello world ")
+
+alert("welcome to js land. . .\nhappy")
+
+
+alert("Hello i can run js through")
+
+// ===========
+// Chapter: 2
+// ===========
+// Q1
+
+var username = "alicoder117"
+ console.log(username)
+
+//  Q2
+
+var myname = "syed muhammed ali "
+console.log(myname)
+
+// Q4
+
+
+let studentName = "ali"
+let studentAge = "17"
+let studentclass = "10"
+let studentrollNumber = "45" 
+let studentcity = "karachi "
+
+
+
+alert(studentName)
+alert(studentAge)
+alert(studentclass)
+alert(studentrollNumber)
+alert(studentcity)
+
+
+
+
+// Q5
+
+
+alert("coding\ncodin\ncodi\ncod\nco\nc")
+
+
+// Q6
+
+var email = "muhammadalisyed207@gmail.com"
+
+alert(email)
+
+// Q7
+
+ var book = " a smarter way to learn javascript "
+ alert ("i am trying to learn from the book" + book )
+
+//  Q8
+
+ document.writeln     ("<h1>My paragraph<\h1>"  )
+ 
+// Q9
+
+ var massage = "Hello world "
+  
+  alert("massage")
+
+  document.writeln (massage)
+
+// ==================
+//   chapter 3      
+// ==================
+
+// Q1
+
+var age =  17
+age = 17
+alert( age)
+
+// Q2
+
+let uservisited = 15
+alert("you have visited this site 15 times ", uservisited, )
+
+
+
+// Q3
+
+ var birthYear = "my birth Year is 17" 
+ document.writeln("<br>",birthYear)
+
+
+// Q4
+
+ var visiterName = "ayesha"
+ var productTitle = "puma"
+ var qty = 5
+document.writeln( "<br>",visiterName,"<br>", productTitle,"<br>",qty)
+
+
+// ========
+//   END 
+// ======== 
