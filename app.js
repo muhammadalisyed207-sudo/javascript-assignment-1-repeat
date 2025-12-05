964fce3b0ed5d82dcb4b1f300817adf28b913635
muhammadalisyed207-sudo <muhammadalisyed207@gmail.com>
2025-12-06 01:10:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// Q1
+
+let ali,hamza,moiz
+
+// Q2
+
+//legal
+let num1,num2,num3,num4,num5
+
+//Illegal
+
+// let 1num,2num,3num,4num,5num
+
+
+// Q3
+// a
+console.log("Rules for naming JS variables:");
+// b 
+console.log("variables names can only contain letters ,$ali_1variable")
+// d 
+let name = Ali
+console.log("Name:");
+// e 
+console.log("variable name shoula not be js methad")
+
+console.log("all vriables naming rules have been displayed sucessfully ")
+
+
+
