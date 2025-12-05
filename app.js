a5f1b48b915fc5d8218f926bd57dcfb5b4107f60
muhammadalisyed207-sudo <muhammadalisyed207@gmail.com>
2025-12-06 01:06:37 +0500

    Add files via upload
    This code shows that the question is from chapters 1 to 3 of the JavaScript book.

---
+++ app.js
@@ -0,0 +1,89 @@
+
+// Q1
+
+// let ali,hamza,moiz
+
+// // Q2
+
+// //legal
+// let num1,num2,num3,num4,num5
+
+// //Illegal
+// // 
+// // let 1num,2num,3num,4num,5num
+
+
+// // Q3
+// // a
+// console.log("Rules for naming JS variables:");
+// // b 
+// console.log("variables names can only contain letters ,$ali_1variable")
+// // d 
+// // let name = Ali
+// // console.log(Name);
+// // e 
+// console.log("variable name shoula not be js methad")
+
+// console.log("all vriables naming rules have been displayed sucessfully ")
+
+
+
+//  num++;
+//  num = num + 1;
+
+// var num = 1;
+//  var newNum = ++num;
+//  var num = 1;
+//  var newNum = num--;
+
+// newNum = 1, num = 2
+
+
+// 
+
+
+// var totalCost = 1 + 3 * 4;
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + userName + banger)
+
+
+
+// var name = "ali"
+// console.log(name)
+
+// var name = "ali"
+// name ="ibrahime"
+// console.log(name)
+
+// var name = "hamza"
+// name = "moiz"
+// console.log(name)
+
+// var name = "pakistan"
+// name = "india"
+// console.log(name)
+
+// var nationality = "pakistan"
+// name= "india";
+
+// var nationality;
+// nationality = "U.S.";
+
+
+
+
+
+// var nationlity = "pakistan"
+// nationlity = "palestine"
+// console.log(nationlity)
+
+var nationality = "pakistan"
+ name = "pakistan"
+ b = "pakistan"
+console.log(nationality);
+
+
+
+
